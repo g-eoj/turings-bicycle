690afb7a9bfdb6d63787636d6d982f6477908a72
Joe G <joe.granados@hotmail.com>
2012-11-29 13:48:21 -0800

    add distance calculation

--- turings-bicycle.py
+++ turings-bicycle.py
@@ -1,20 +1,26 @@
-#wheel_diameter = raw_input("What's the rear wheel's diameter in meters?: ")
+from math import pi
+
+tire_diameter = float(raw_input("What's the rear tire's outer diameter in milimeters?: "))
 chain_total_links = int(raw_input("How many links on the chain?: "))
 sprocket_total_teeth = int(raw_input("How many teeth on the sprocket?: "))
 #chain_weak_link = 0 # starting position of the weak link relative to the sprocket
-#wheel_bent_spoke = 0 # starting position of the bent spoke in degrees
+#rim_bent_spoke = 0 # starting position of the bent spoke in degrees relative to the sprocket
+tire_circumference = tire_diameter * pi
 chain_position = 0
 sprocket_revolutions = 0
-#distance_traveled = # kilometers?
-#if distance_traveled > 1.0:
-#  units = 'kilometers'
-#else:
-#  units = 'meters'
-#  distance_traveled *= 100
+distance_traveled = 0.0
+units = 'milimeters'
 while sprocket_revolutions < 201:
   sprocket_revolutions += 1
   chain_position = (chain_position + sprocket_total_teeth) % chain_total_links
+  distance_traveled += tire_circumference
   if chain_position == 0:
-  #  print 'The bike can travel %d %s before the chain will fall off.' % (distance_traveled, units)
-    print 'The chain fell off after %d sprocket revolutions' % sprocket_revolutions
+    if distance_traveled > 1000:
+      units = 'meters'
+      distance_traveled /= 1000
+    if distance_traveled > 1000:
+      units = 'kilometers'
+      distance_traveled /= 1000
+    print 'The bike can travel %.2f %s before the chain will fall off.' % (distance_traveled, units)
+    #print 'The chain fell off after %d sprocket revolutions' % sprocket_revolutions
     break
